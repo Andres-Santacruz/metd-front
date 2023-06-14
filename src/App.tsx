86fb09375d07ec5bc6f15d89ebe9f884640d226a
andres santacruz <andres_v10@live.com>
2023-06-13 19:26:12 -0500

    [FEATURE]: Add lottie

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,31 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { useGetUser } from "./hooks/useGetUser";
 import LoginPage from "./pages/LoginPage";
 import AdminLayout from "./layouts/AdminLayout";
+import { Box, Flex, Text } from "@chakra-ui/react";
+
+import Lottie from "lottie-react";
+import groovyWalkAnimation from "./assets/img/lotties/validatingLottie.json";
 
 function App() {
   const { checking } = useGetUser();
 
-  if (checking === "loading") return <div>loading</div>;
+  if (checking === "loading") {
+    return (
+      <Flex
+        justifyContent="center"
+        alignItems="center"
+        height="100vh"
+        bg="#ffffff"
+      >
+        <Box maxW="400px">
+          <Text textAlign="center" color="gray.400">
+            Validando información...
+          </Text>
+          <Lottie animationData={groovyWalkAnimation} loop={true} />
+        </Box>
+      </Flex>
+    );
+  }
   if (checking === "no-user") {
     return (
       <Routes>
